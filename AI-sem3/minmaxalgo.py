0d504d25426ddeaf2d8e905c89afdfc1d7be1b0b
Sinhakoustubh <koustubhsinhapolytecnic@gmail.com>
2025-07-31 23:52:59 +0530

    Upgraded myself to AI engineer :-)

--- AI-sem3/minmaxalgo.py
+++ AI-sem3/minmaxalgo.py
@@ -15,16 +15,16 @@ def showGrid():
         if i<2: print("---------")
 
 ### Check for available moves
-def avalMove():
+def avalMove(grid):
     lst = []
-    for row in grid:
-        if grid[row][0] == " ":lst.append((row,0))
-        if grid[row][1] == " ":lst.append((row,1))
-        if grid[row][2] == " ":lst.append((row,2))
+    for rowN in range(3):
+        if grid[rowN][0] == " ":lst.append((rowN,0))
+        if grid[rowN][1] == " ":lst.append((rowN,1))
+        if grid[rowN][2] == " ":lst.append((rowN,2))
     return lst
 
 ### Check for winner
-def winner(contender):
+def winner(contender, grid):
     for i in range(3):
         if grid[i][0] == grid[i][1] == grid[i][2] == contender: return True
         if grid[0][i] == grid[1][i] == grid[2][i] == contender: return True
@@ -32,44 +32,44 @@ def winner(contender):
     if grid[0][0] == grid[1][1] == grid[2][2] == contender: return True
     return False
 
-def checkWin():
-    if winner(player): return player
-    if winner(comptr): return comptr
-    if avalMove() == []: return "DRAW"
+def checkWin(grid):
+    if winner(player, grid): return player
+    if winner(comptr, grid): return comptr
+    if avalMove(grid) == []: return "DRAW"
     return None
 
 ### Minmax Algorithm
-def minmaxAlgo(grid, isMaxmize):
-    result = checkWin()
+def minmaxAlgo(grid_local, isMaxmize):
+    result = checkWin(grid_local)
     if result == player: return -1
     elif result == comptr: return 1
     elif result == 'DRAW': return 0
 
     if isMaxmize:                               # maximize function for computer turn
         bestScore = -math.inf
-        for cord in avalMove():
-            grid[cord[0]][cord[1]] = comptr     # make hypothetical move
-            score = minmaxAlgo(grid, False)           # recurse for player's turn
-            grid[cord[0]][cord[1]] = " "        # backtrack move
+        for cord in avalMove(grid_local):
+            grid_local[cord[0]][cord[1]] = comptr     # make hypothetical move
+            score = minmaxAlgo(grid_local, False)           # recurse for player's turn
+            grid_local[cord[0]][cord[1]] = " "        # backtrack move
             bestScore = max(score, bestScore)
         return bestScore
     else:                   # isMaximize = False; minimize function for player turn
         bestScore = math.inf
-        for cord in avalMove():
-            grid[cord[0]][cord[1]] = player
-            score = minmaxAlgo(grid, True)
-            grid[cord[0]][cord[1]] = " "
+        for cord in avalMove(grid_local):
+            grid_local[cord[0]][cord[1]] = player
+            score = minmaxAlgo(grid_local, True)
+            grid_local[cord[0]][cord[1]] = " "
             bestScore = min(score, bestScore)
         return bestScore
     
-def bestAImove(grid):
+def bestAImove(grid_local):
     bestScore = -math.inf
     bestMove = None
 
-    for cord in avalMove():
-        grid[cord[0]][cord[1]] = comptr     # make hypothetical move
-        score = minmaxAlgo(grid, False)           # recurse for player's turn
-        grid[cord[0]][cord[1]] = " "        # backtrack move
+    for cord in avalMove(grid_local):
+        grid_local[cord[0]][cord[1]] = comptr     # make hypothetical move
+        score = minmaxAlgo(grid_local, False)           # recurse for player's turn
+        grid_local[cord[0]][cord[1]] = " "        # backtrack move
         
         if bestScore < score:
             bestScore = score
@@ -98,8 +98,16 @@ showGrid()
 
 while True:
     # Human Player's turn
-    R = input("Enter row (0-2):")
-    C = input("Enter column (0-2):")
+    try:
+        R = int(input("Enter row (0-2):"))
+        C = int(input("Enter column (0-2):"))
+    except:
+        print("Invalid input!")
+        continue
+
+    if R<0 or R>2 or C<0 or C>2:
+        print("Input exceeds the grid size. Please re-enter!")
+        continue
 
     if grid[R][C] != " ":
         print("The block is unavailable, Please choose again!")
@@ -107,7 +115,7 @@ while True:
     grid[R][C] = player
     showGrid()
 
-    finalResult = checkWin() 
+    finalResult = checkWin(grid) 
     if finalResult != None: break
 
     # Computer's turn
@@ -116,7 +124,7 @@ while True:
         grid[AIMove[0]][AIMove[1]] = comptr
     showGrid()
 
-    finalResult = checkWin() 
+    finalResult = checkWin(grid) 
     if finalResult != None: break
 
 if finalResult == player:
